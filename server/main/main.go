e66dfc266febaa0b0171a1d473c9397628ea40c1
noamlerner <noaml1295@gmail.com>
2023-09-14 23:28:50 -0700

    runcasino

--- server/main/main.go
+++ server/main/main.go
@@ -1,26 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"net"
-
-	"github.com/noamlerner/TheGamblr/proto/thegamblr/proto"
 	"github.com/noamlerner/TheGamblr/server"
-
-	"google.golang.org/grpc"
 )
 
 func main() {
-	gs := grpc.NewServer()
-	proto.RegisterCasinoServer(gs, server.NewCasinoServer())
-	fmt.Printf("starting server at port 8080")
-	listen, err := net.Listen("tcp", "localhost:8080")
-	if err != nil {
-		panic(err)
-	}
-
-	err = gs.Serve(listen)
-	if err != nil {
-		panic(err)
-	}
+	server.RunCasino()
 }
