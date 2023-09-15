19adebf95af583029f2926910f4b2a5526ae5613
noamlerner <noaml1295@gmail.com>
2023-09-14 23:28:50 -0700

    readme

--- client/main/main.go
+++ client/main/main.go
@@ -8,16 +8,26 @@ import (
 )
 
 func main() {
+	// ClientBot takes an algorithm that implements engine.BotPlayer and handles the logic of talking to the casino.
+	// Here, we are creating to ClientBots that take random actions.
 	bot1 := client.NewClientBot("Bean", engine.NewRandomActionBot(), "localhost:8080")
 	bot2 := client.NewClientBot("Noam", engine.NewRandomActionBot(), "localhost:8080")
+
 	ctx := context.Background()
+	// bot1 creates a new game and then both bot1 and bot2 join the game.
 	gameID := bot1.CreateGame(ctx)
 	bot1.JoinGame(ctx, gameID)
 	bot2.JoinGame(ctx, gameID)
 
+	// Bot1 starts the game
 	bot1.StartGame(ctx)
+
+	// async, bot1 starts playing. We do this async because there is a while loop in here, and bot2 also needs to start
+	// playing.
 	go func() {
 		bot1.Run(ctx)
 	}()
+
+	// bot2 starts playing. We do not do this async so that the program doesn't end.
 	bot2.Run(ctx)
 }
---
+++ client/protoconversion.go
@@ -0,0 +1,41 @@
+package client
+
+import (
+	"pokerengine/engine"
+	"pokerengine/proto/thegamblr/proto"
+)
+
+type _protoConv struct{}
+
+var protoConv = &_protoConv{}
+
+func (p *_protoConv) convertProtoPlayer(player *proto.PlayerState) engine.PlayerState {
+	if player == nil {
+		return nil
+	}
+	var roundResults engine.PlayerRoundResults
+	if player.GetRoundResults() != nil {
+		pbResults := player.RoundResults
+		cards := p.convertProtoCards(pbResults.Cards)
+		roundResults = engine.NewPlayerRoundResults(int(pbResults.ChipsWon), cards, engine.HandStrength(pbResults.HandStrength))
+	}
+
+	return engine.NewPlayerState(int(player.GetStack()), engine.PlayerStatus(player.GetStatus()), int(player.GetSeatNumber()), player.GetId(), roundResults)
+}
+
+func (p *_protoConv) convertProtoCards(pbCards []*proto.Card) engine.Cards {
+	cards := make(engine.Cards, len(pbCards))
+	for i, card := range pbCards {
+		cards[i] = engine.NewCard(engine.Rank(card.Rank), engine.Suit(card.Suit))
+	}
+	return cards
+}
+
+func (p *_protoConv) convertBoard(board *proto.BoardState) engine.BoardState {
+	players := make([]engine.PlayerState, len(board.GetPlayers()))
+	for i, player := range board.GetPlayers() {
+		players[i] = p.convertProtoPlayer(player)
+	}
+	boardState := engine.NewBoardState(p.convertProtoCards(board.CommunityCards), int(board.Pot), engine.Stage(board.Stage), int(board.SmallBlindButton), players)
+	return boardState
+}
